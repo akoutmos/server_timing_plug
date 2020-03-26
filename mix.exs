c9f92963f4b54f0106e8810f8a7bc8cfa395d97a
Alex Koutmos <akoutmos@gmail.com>
2020-03-26 01:01:32 -0400

    Lowering Elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ServerTimingPlug.MixProject do
     [
       app: :server_timing_plug,
       version: "0.1.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
